ff3795986feec583edbb4ae4efecdf9c16e91417
arinakh2005 <arinakholodnytska3@gmail.com>
2023-08-08 19:20:04 +0300

    fix: handle notOnlyWhiteSpace trim error

--- src/app/validators/luv2-shop-validators.ts
+++ src/app/validators/luv2-shop-validators.ts
@@ -4,7 +4,7 @@ export class Luv2ShopValidators {
   public static notOnlyWhiteSpace(control: FormControl): ValidationErrors | null {
     const controlValue = control.value;
     const isEmptyControlValue = controlValue === null;
-    const isFilledByWhiteSpace = controlValue.trim().length === 0;
+    const isFilledByWhiteSpace = controlValue?.trim().length === 0;
     const isFailedByValidator = (!isEmptyControlValue && isFilledByWhiteSpace);
 
     return isFailedByValidator
